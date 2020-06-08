46bc32aaeae5b016a69b42f507340fe04fee0dd4
Nanda Gopal <ngopal253@gmail.com>
2020-06-08 22:31:30 +0530

    auth done

--- typeDefs/auth.js
+++ typeDefs/auth.js
@@ -1,7 +1,14 @@
 const { gql } = require('apollo-server-express');
 
 module.exports = gql`
+  type UserCreateResponse {
+    username: String!
+    email: String!
+  }
   type Query {
     me: String!
+  }
+  type Mutation {
+    createUser: UserCreateResponse!
   }
 `;
